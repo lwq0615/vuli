1989596a201fe6e3afc57314e6047ddacc84ad5d
lwq <1072864729@qq.com>
2022-02-23 16:48:56 +0800

    'update'

--- examples/main.js
+++ examples/main.js
@@ -2,9 +2,9 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import '../packages/css/index.scss'
-import luckUi from '../packages/lib/index.js'
+import Vui from '../packages/lib/index.js'
 
-Vue.use(luckUi)
+Vue.use(Vui)
 
 Vue.config.productionTip = false
 
