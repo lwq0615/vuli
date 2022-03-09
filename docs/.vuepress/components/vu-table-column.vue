90ac2122cba3bd91d42bb90a092c217e52614c74
lwq <1072864729@qq.com>
2022-03-09 19:50:22 +0800

    update

--- docs/.vuepress/components/vu-table-column.vue
+++ docs/.vuepress/components/vu-table-column.vue
@@ -91,9 +91,6 @@ export default {
             this.$nextTick(() => {
                 this.refresh = true
                 this.$nextTick(() => {
-                    console.log(this.parentTr.domObj)
-                    console.log(this.parentTr.domObj[this.prop])
-                    console.log(this.$el)
                     this.parentTr.domObj[this.prop].appendChild(this.$el)
                 })
             })
